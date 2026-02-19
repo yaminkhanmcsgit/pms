55046adeedf79948c8c7e732d3b2d0a126b721e2
yaminkhanmcs <yaminkhanmcs@gmail.com>
2026-02-19 10:40:55 +0500

    no message

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://slrdirkalam.com', 'https://www.slrdirkalam.com'],
 
     'allowed_origins_patterns' => [],
 
